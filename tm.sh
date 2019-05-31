e977f0ad158dd575d78c1e6e0ce7f6cb40495809
neverxie <neverxie@outlook.com>
2019-05-31 13:40:41 +0800

    添加bashrc；添加gitconfig；调整tmux启动界面；调整tmux面板切换快捷键；去掉vimrc里不常用的插件；

--- tm.sh
+++ tm.sh
@@ -8,10 +8,23 @@ if [ $? = 0 ];then
     exit
 fi
 
-tmux new-session -d -s $session -n scons
-tmux new-window -t $session:2 -n edit
-tmux new-window -t $session:3 -n openocd
-tmux new-window -t $session:4 -n minicom
+tmux new-session -d -s $session -n bash
+tmux split-window -h
+tmux select-pane -t 2
+tmux split-window -v
+
+## 竖直分割
+#tmux split-window -h
+#tmux select-pane -t 1
+#tmux split-window -v
+## 将窗口分为上下两部分，并通过-p指定比例
+#tmux split-window -v -p 40
+
+tmux new-window -t $session:2 -n vim
+
+# 这个暂时先不要了
+#tmux new-window -t $session:3 -n minicom
 
 tmux select-window -t $session:1
+tmux select-pane -t 1
 tmux attach-session -t $session
