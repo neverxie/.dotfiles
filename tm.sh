4630e315cbd3871f89ed5c4694155a5a6927103a
lepustse <lepus.tse@gmail.com>
2021-01-16 15:07:24 +0800

    [m]修改tmux启动脚本

--- tm.sh
+++ tm.sh
@@ -2,6 +2,9 @@
 
 session=$1
 
+# 兼容zsh
+export DISABLE_AUTO_TITLE="true"
+
 # 判断是否存在 $session 会话
 tmux has-session -t $session
 if [ $? = 0 ];then
@@ -10,11 +13,12 @@ if [ $? = 0 ];then
 fi
 
 # 创建一个新session名称为 $session，新窗口名为bash，并detach
-tmux new-session -d -s $session -n bash
+tmux new-session -d -s $session -n debug
 tmux split-window -h
-tmux select-pane -t 2
+tmux split-window -v -p 10 # 窗口分为上下两部分，并通过-p制定比例
+#tmux select-pane -t 2
 # 将窗口分为上下两部分
-tmux split-window -v
+#tmux split-window -v
 
 ## 竖直分割
 #tmux split-window -h
@@ -24,12 +28,13 @@ tmux split-window -v
 #tmux split-window -v -p 40
 
 # 创建一个新的窗口
-tmux new-window -t $session:2 -n bash
+tmux new-window -t $session:2 -n code
 
 # 这个暂时先不要了
 #tmux new-window -t $session:3 -n minicom
 
 # 选择一个默认窗口
+tmux send-keys -t $session:1 'zsh' C-m
 tmux select-window -t $session:1
 tmux select-pane -t 1
 tmux attach-session -t $session
